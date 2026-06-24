373c6a84b2c776e42b54e149386e4da55efe441b
GustavoRode <gustavo.rode@gmail.com>
2026-06-24 13:06:31 -0300

    Add Not Found Middleware

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from "express";
+import notFound from "./src/middlewares/not-found.js"
+
 const app = express()
 
+app.use((req,res, next) => {
+    //res.json({message: 'Soy Middleware'})
+    console.log(req.method)
+    next()
+})
+
 app.get('/',(req,res) =>{
-  res.send("¡Welcome to API Rest!")
+  res.send({message: "¡Welcome to API Rest!"})
 
 })
 
-const PORT = 3000
+
+
+app.use(notFound)
+const PORT = 3000   
 
 app.listen(PORT,() => console.log(`http://localhost:${PORT}`));
 
